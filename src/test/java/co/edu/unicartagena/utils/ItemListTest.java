63b4b1fcc762dcc0751f78f1a5dcd6dfb6bd0971
Pablo Hernández <74122319+pablohernandezm@users.noreply.github.com>
2023-09-21 11:48:53 -0500

    Actualizados los test

--- src/test/java/co/edu/unicartagena/utils/ItemListTest.java
+++ src/test/java/co/edu/unicartagena/utils/ItemListTest.java
@@ -3,13 +3,51 @@ package co.edu.unicartagena.utils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
 import java.nio.file.Files;
 
 public class ItemListTest {
 
   @Test
+  public void crearLista(){
+    ItemList lista = new ItemList();
+    Assertions.assertNotNull(lista, "La lista no se creó");
+  }
+
+  @Test
+  public void agregar(){
+    ItemList lista = new ItemList();
+
+    Assertions.assertDoesNotThrow(() -> lista.add(RandomStringUtils.randomAlphanumeric(8),
+            "GeForce RTX 4090", "Nvidia",
+            Producto.Tipo.ELECTRONICA_OFICINA,
+            13999000), "No se pudo agregar el producto");
+
+    Assertions.assertEquals(1, lista.size(), "El tamaño de la lista no es correcto");
+  }
+
+  @Test
+  public void borrar(){
+    ItemList lista = new ItemList();
+
+    String id1 = RandomStringUtils.randomAlphanumeric(8);
+    lista.add(id1, "GeForce RTX 4090", "Nvidia", Producto.Tipo.ELECTRONICA_OFICINA, 13999000);
+
+    String id2 = RandomStringUtils.randomAlphanumeric(8);
+    lista.add(id2, "GeForce RTX 3050", "Nvidia", Producto.Tipo.ELECTRONICA_OFICINA, 3299000);
+
+    Assertions.assertDoesNotThrow(() -> lista.borrarItem(id1), "No se pudo borrar el producto");
+    Assertions.assertEquals(1, lista.size(), "El tamaño de la lista no es correcto");
+
+    Assertions.assertDoesNotThrow(() -> lista.borrarItem(id2), "No se pudo borrar el producto");
+    Assertions.assertEquals(0, lista.size(), "El tamaño de la lista no es correcto");
+  }
+
+  @Test
   public void guardarItems(){
     ItemList lista = new ItemList();
+
     String[] productos = {"papa", "arroz", "frijol", "carne", "pollo", "pescado", "leche", "huevos", "queso", "yogurt"};
     String[] proveedores = {"Alpina", "Colanta", "Colombina", "Diana", "Zenú", "Pilgrim's", "Bavaria", "Coca-Cola", "Pepsi", "Postobón"};
 
@@ -32,6 +70,14 @@ public class ItemListTest {
   @Test
   public void cargarItems(){
     ItemList lista = new ItemList();
-    Assertions.assertDoesNotThrow(lista::cargarItems, "No se pudo cargar el archivo");
+    lista.guardarItems();
+    Assertions.assertThrows(FileNotFoundException.class, lista::cargarItems, "No se pudo cargar el archivo");
+
+    guardarItems();
+    Assertions.assertDoesNotThrow(lista::cargarItems, "No se pudo cargar el archivo luego de ser cargado y guardado.");
+
+    Assertions.assertNotEquals(lista.size(), 0, "La lista no se cargó correctamente.");
+    Assertions.assertDoesNotThrow(lista::cargarItems, "No se pudo cargar el archivo luego de ser cargado anteriormente.");
+
   }
 }
